bde94390f3758b38778cc043794741dac4acc070
SBHAMRV <SBHAMRV@DESKTOP-H51FCSG>
2021-10-09 04:18:51 +0400

    Add project files.

---
+++ TeacherTest/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using testDll;
+using pdTcpIpService;
+using System.Collections.Generic;
+
+
+
+namespace TeacherTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //TestCs tstCs = new TestCs();
+
+            //tstCs.setName("Sabuhi");
+            //tstCs.setName("Cavid");
+            //tstCs.setName("Ferid");
+
+
+            //  Console.WriteLine(tstCs.getId());
+            //Console.WriteLine(tstCs.getName());
+
+            //Console.WriteLine(tstCs.lastName);
+
+              MyList<string> myList = new MyList<string>();
+
+            myList.Add("Sabuhi");
+
+            Console.WriteLine(myList.Length);
+
+            myList.Add("Isi");
+
+            Console.WriteLine(myList.Length);
+
+            foreach (var item in myList.Items)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+
+    class TestCs
+    {
+        string _name;
+        int _id;
+        string _lastName;
+
+       
+        public int getId()
+        {
+            return _id;
+        }
+        public string getName()
+        {
+            return "Mr: "+ _name;
+        }
+
+        public void setName(string name)
+        {
+            this._id++;
+            _name = name;
+        }
+
+    }
+}
---
+++ TeacherTest/TestDll.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeacherTest
+{
+    public  class TestDll
+    {
+     
+        private int _a; //field
+        private int _b;  //field
+
+        public void setA(int a)
+        {
+            _a = a;
+        }
+
+        private void Sum()
+        {
+            Console.WriteLine(_a+_b);
+        }
+    }
+   
+}
